dcbb4f5c7dd9b2d84de77998f53881d835a99fc8
Abdelilah_OU <gxgamer710@gmail.com>
2023-01-03 18:39:58 +0000

    work on client pinia store

--- client/src/stores/clientStore.ts
+++ client/src/stores/clientStore.ts
@@ -1,4 +1,10 @@
-import type { client, clientState, dataRows } from "@/types";
+import type {
+  client,
+  dataRow,
+  dataRows,
+  clientState,
+  updateClient,
+} from "@/types";
 import { defineStore } from "pinia";
 import axios from "axios";
 
@@ -21,6 +27,18 @@ export const useClientStore = defineStore("ClientStore", {
     getAllClients: async function () {
       const res: dataRows<client> = await axios.get(api);
       console.log(res.data.rows);
+    },
+    getOneClient: async function (id: number) {},
+    deleteOneClient: async function (id: number) {
+      const res: { data: { msg: string } } = await axios.delete(api + id);
+      console.log(res);
+    },
+    updateOneClient: async function (id: number, client: updateClient) {
+      const res: dataRow<client> = await axios.post(api + id, {
+        body: {
+          client,
+        },
+      });
     },
   },
 });
